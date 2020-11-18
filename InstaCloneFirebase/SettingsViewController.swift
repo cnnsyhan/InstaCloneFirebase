0f031fa7ac6f946b1fa4e0ed15566eab77da05fc
Can Seyhan <muhamedseyhan503@gmail.com>
2020-11-18 12:22:29 +0300

    Until Sign In

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Can Seyhan on 11/15/20.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+
+}
