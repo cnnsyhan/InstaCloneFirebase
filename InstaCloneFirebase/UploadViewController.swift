d7f6229fe62257bc00032645e8d464a532f33cfe
Can Seyhan <muhamedseyhan503@gmail.com>
2020-11-20 18:51:31 +0300

    Full Project - Final

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -9,19 +9,20 @@ import UIKit
 import Firebase
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var commentText: UITextField!
+    
     @IBOutlet weak var uploadButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         imageView.isUserInteractionEnabled = true
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
         imageView.addGestureRecognizer(gestureRecognizer)
-
-        // Do any additional setup after loading the view.
+        
     }
     
     @objc func chooseImage() {
@@ -33,38 +34,43 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         
     }
     
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
     }
     
     func makeAlert(titleInput: String, messageInput: String) {
-           let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
-           let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-           alert.addAction(okButton)
-           self.present(alert, animated: true, completion: nil)
-    
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
+
     @IBAction func actionButtonClicked(_ sender: Any) {
         
+        
         let storage = Storage.storage()
-        let storageReferance = storage.reference()
+        let storageReference = storage.reference()
         
-        let mediaFolder = storageReferance.child("Media")
+        let mediaFolder = storageReference.child("media")
         
         
-        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
             let uuid = UUID().uuidString
-        
-            let imageReferance = mediaFolder.child("\(uuid).jpg")
-            imageReferance.putData(data, metadata: nil) { (metadata, error) in
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { (metadata, error) in
                 if error != nil {
                     self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
                 } else {
                     
-                    imageReferance.downloadURL { (url, error) in
+                    imageReference.downloadURL { (url, error) in
+                        
                         if error == nil {
+                            
                             let imageUrl = url?.absoluteString
                             
                             
@@ -72,13 +78,15 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             let firestoreDatabase = Firestore.firestore()
                             
-                            var firestoreReferance : DocumentReference? = nil
+                            var firestoreReference : DocumentReference? = nil
                             
-                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!, "date" : FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
-                            
-                            firestoreReferance = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
+                            let firestorePost = ["imageUrl" : imageUrl!, "postedBy" : Auth.auth().currentUser!.email!, "postComment" : self.commentText.text!,"date" : FieldValue.serverTimestamp(), "likes" : 0 ] as [String : Any]
+
+                            firestoreReference = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { (error) in
                                 if error != nil {
-                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                                    
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                    
                                 } else {
                                     
                                     self.imageView.image = UIImage(named: "select.png")
@@ -90,16 +98,17 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
                             
                             
                             
-                            
                         }
+                        
+                        
                     }
                     
                 }
             }
             
+            
         }
         
     }
     
-
 }
